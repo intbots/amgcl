741d9452e55a8675ba19b3593870e877e7eaaefd
Denis Demidov <ddemidov@ksu.ru>
2013-03-25 15:55:09 +0400

    Read matrix, convert it to block format

--- examples/block.cpp
+++ examples/block.cpp
@@ -4,6 +4,7 @@
 
 #include <Eigen/Dense>
 #include <Eigen/SparseCore>
+#include <unsupported/Eigen/SparseExtra>
 
 #include <amgcl/operations_eigen.hpp>
 #include <amgcl/amgcl.hpp>
@@ -24,14 +25,65 @@ typedef Eigen::Matrix<vblock, Eigen::Dynamic, 1> EigenVector;
 int main(int argc, char *argv[]) {
     amgcl::profiler<> prof(argv[0]);
 
-    // Read matrix and rhs from a binary file.
-    std::vector<int>    row;
+    // Read matrix in general format.
+    Eigen::SparseMatrix<real, Eigen::RowMajor, int> GenA;
+    Eigen::loadMarket(GenA, argv[1]);
+
+    // Convert it to block format;
+    int n = GenA.rows() / 4;
+    std::vector<int>    row(n + 1, 0);
     std::vector<int>    col;
     std::vector<mblock> val;
-    EigenVector         rhs;
 
-    // TODO: Init matrix
-    int n = 100;
+    {
+        std::vector<int> marker(n, -1);
+        for(int i = 0; i < GenA.rows(); ++i) {
+            int ii = i / 4;
+            for(int j = GenA.outerIndexPtr()[i], e = GenA.outerIndexPtr()[i + 1]; j < e; ++j) {
+                int jj = GenA.innerIndexPtr()[j] / 4;
+                if (marker[jj] != ii) {
+                    marker[jj] = ii;
+                    row[ii + 1]++;
+                }
+            }
+        }
+
+        std::partial_sum(row.begin(), row.end(), row.begin());
+        std::fill(marker.begin(), marker.end(), -1);
+
+        col.resize(row.back());
+        val.resize(row.back());
+        for(int i = 0; i < GenA.rows(); ++i) {
+            int gi = i / 4;
+            int li = i % 4;
+            int row_head = row[gi];
+            int row_tail = row_tail;
+            for(int j = GenA.outerIndexPtr()[i], e = GenA.outerIndexPtr()[i + 1]; j < e; ++j) {
+                int gj = GenA.innerIndexPtr()[j] / 4;
+                int lj = GenA.innerIndexPtr()[j] % 4;
+
+                if (marker[gj] < row_head) {
+                    marker[gj] = row_tail;
+
+                    col[row_tail] = gj;
+                    val[row_tail](li,lj) = GenA.valuePtr()[j];
+
+                    row_tail++;
+                } else {
+                    val[marker[gj]](li,lj) = GenA.valuePtr()[j];
+                }
+            }
+        }
+
+    }
+
+    EigenVector rhs(n), x(n);
+    rhs.resize(n);
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < 4; j++) {
+            rhs[i][j] = 1;
+            x[i][j] = 0;
+        }
 
     // Wrap the matrix into Eigen Map.
     Eigen::MappedSparseMatrix<mblock, Eigen::RowMajor, int> A(
@@ -56,7 +108,6 @@ int main(int argc, char *argv[]) {
     std::cout << amg << std::endl;
 
     // Solve the problem with CG method. Use AMG as a preconditioner:
-    EigenVector x(n);// = EigenVector::Zero(n);
     prof.tic("solve (cg)");
     //std::pair<int,double> cnv = amgcl::solve(A, rhs, amg, x, amgcl::cg_tag());
     std::pair<int,double> cnv = amg.solve(rhs, x);
